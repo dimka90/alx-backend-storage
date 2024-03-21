2911064cb0ce1777efd47d91183ccd5b7091e9f0
dimka90 <dimkayilrit@gmail.com>
2024-03-21 15:56:42 +0100

    created a users table in the holberton database that has an enum type

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,13 @@
+-- Task: Create a table named users with specified attributes
+
+-- Create table users with specified attributes
+CREATE TABLE IF NOT EXISTS users (
+    -- Define id as an integer, auto_increment and a primary key
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    -- Define email as a string , never null
+    email VARCHAR(255) NOT NULL,
+    -- Define name as a string
+    name  VARCHAR(255),
+    -- Define country as an ENUM type , never NULL with a default value
+    country ENUM('US','CO','TN') NOT NULL DEFAULT 'US'
+);
