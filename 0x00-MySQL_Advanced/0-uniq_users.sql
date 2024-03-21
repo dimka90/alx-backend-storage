eb993a20a7df42f4c5eeea40e5c03c76fee6190b
dimka90 <dimkayilrit@gmail.com>
2024-03-21 15:06:12 +0100

    added comments to each query

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,7 +1,11 @@
 -- An Sql script that creates a table users following these requirements
 
+-- Create table users with specified attributes
 CREATE TABLE   IF NOT EXISTS users(
+-- Define id as an integer, auto increment, and primary key
 id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+-- Define email as a string of 255 characters, never null, and unique
 email VARCHAR(255) NOT NULL UNIQUE,
+-- Define name as a string of 255 characters
 name VARCHAR(255) NOT NULL 
 );
