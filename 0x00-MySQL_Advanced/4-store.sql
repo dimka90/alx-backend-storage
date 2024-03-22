b75cb5393a6da62ea16f8504ce5ef9a636a67db2
dimka90 <dimkayilrit@gmail.com>
2024-03-22 05:18:34 +0100

    fix more bug

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -2,13 +2,10 @@
 CREATE TRIGGER decrease_item_quantity_in_stock AFTER INSERT ON orders
 -- Looping through each row
 FOR EACH ROW
--- start
-BEGIN
-    -- Update the quantity of the corresponding item in the items table
-    UPDATE items
-    -- Subtract the quantity of the new order from the current item quantity
-    SET quantity = quantity - NEW.number
-    -- Match the item name of the new order with the item name in the items table
-    WHERE name = NEW.item_name
-END;
+-- Update the quantity of the corresponding item in the items table
+UPDATE items
+-- Subtract the quantity of the new order from the current item quantity
+SET quantity = quantity - NEW.number
+-- Match the item name of the new order with the item name in the items table
+WHERE name = NEW.item_name;
 
