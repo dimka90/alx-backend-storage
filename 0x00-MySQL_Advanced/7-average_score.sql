acc28f7a62dfbd0509ba4b66bf63174ae0935243
dimka90 <dimkayilrit@gmail.com>
2024-03-24 05:49:59 +0100

    fix some bug on task 7

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -1,16 +1,18 @@
--- Change the delimiter to $$ to handle the procedure definition
-DELIMITER $$;
 
--- Create a stored procedure named ComputeAverageScoreForUser, taking user_id as input
-CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
-BEGIN 
-    -- Update the users table, setting the average_score column to the result of the subquery
-    UPDATE users
-    SET 
-    average_score = (SELECT AVG(score) FROM corrections WHERE corrections.user_id = user_id);
-    -- Filter the rows to update to match the user_id provided
-    WHERE id = user_id;
-END $$;
+-- Create stored procedure ComputeAverageScoreForUser
+-- computes and stores average score for a student
+-- Takes 1 input user_id, a users.id
 
--- Reset the delimiter back to the default semicolon
+DELIMITER $$
+
+DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
+CREATE PROCEDURE ComputeAverageScoreForUser (IN user_id INT)
+BEGIN
+	UPDATE users
+	SET
+	average_score = (SELECT AVG(score) FROM corrections WHERE corrections.user_id = user_id)
+	WHERE id = user_id;
+
+END $$
+
 DELIMITER ;
