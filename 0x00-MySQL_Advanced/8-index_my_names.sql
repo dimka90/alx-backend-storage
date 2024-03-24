0276a47a39f64f01baea7eb2755e9565d0376155
dimka90 <dimkayilrit@gmail.com>
2024-03-24 05:16:38 +0100

    fix some bug

--- 0x00-MySQL_Advanced/8-index_my_names.sql
+++ 0x00-MySQL_Advanced/8-index_my_names.sql
@@ -1,3 +1,4 @@
  -- Creating an index on the first letter on a name
  -- Only first letter of name must be indexed
-CREATE INDEX idx_name_first ON names(LEFT(name,1));
+CREATE INDEX idx_name_first ON 
+names(name(1));
