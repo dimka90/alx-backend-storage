6a37b84824cb554c7c06e4031547546d6050492e
dimka90 <dimkayilrit@gmail.com>
2024-03-24 05:52:27 +0100

    corrected the emaul field to unique on task 1

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -5,7 +5,7 @@ CREATE TABLE IF NOT EXISTS users (
     -- Define id as an integer, auto_increment and a primary key
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     -- Define email as a string , never null
-    email VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
     -- Define name as a string
     name  VARCHAR(255),
     -- Define country as an ENUM type , never NULL with a default value
