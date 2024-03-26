0e1c2c608691572c03ea3375be9e21f53390a436
dimka90 <dimkayilrit@gmail.com>
2024-03-26 15:09:28 +0100

    created a script that update a collection

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+"""
+"""
+
+
+def update_topics(mongo_collection, name, topics):
+    """
+    A function that adds new field to an already exiting collection
+    Argument:
+            mongo_collection(mongo Object): a mongo object
+            name(string): name of the school
+            topics(list): an array of topics
+    """
+    mongo_collection.update_many({"name": name}, {"$set": {"topics": topics}})
